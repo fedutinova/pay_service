a7888206b4d45a2d49bc67e29d45a6555df3f051
Gleb <gleb.pekhov@inplatlabs.ru>
2024-12-05 18:43:03 +0300

    docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,27 @@
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+import pytest
 import pytest_asyncio
 
+from sqlalchemy import MetaData
 from testcontainers.postgres import PostgresContainer
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from alembic.config import Config
-
+from app.app import get_app
 
 from app.db import Base
-
+collect_ignore = ["app/logger.py", "another_ignored_file.py"]
 
 @pytest_asyncio.fixture(scope="session")
 async def db():
     postgres = PostgresContainer("postgres:16-alpine", driver="asyncpg")
     postgres.start()
+    engine = create_async_engine(postgres.get_connection_url(), echo=True)
+    alembic_cfg = Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", postgres.get_connection_url())
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
     yield postgres
     postgres.stop()
 
@@ -23,11 +32,9 @@ async def prepare_database(db: PostgresContainer):
     alembic_cfg = Config("alembic.ini")
     alembic_cfg.set_main_option("sqlalchemy.url", db.get_connection_url())
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-    await engine.dispose()
+        await conn.run_sync(Base.metadata.reflect)
+        for table in reversed(Base.metadata.sorted_tables):
+            await conn.execute(table.delete())
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -36,3 +43,9 @@ async def session(db):
     session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with session_maker() as session:
         yield session
+
+
+@pytest_asyncio.fixture(scope="function")
+async def client():
+    async with AsyncClient(app=get_app(), base_url="http://test") as client:
+        yield client
--- tests/repositories/test_company_repo.py
+++ tests/repositories/test_company_repo.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 from app.repositories.company_repo import company_repo
 from app.models.companies_model import Company
 
+
 @pytest.mark.asyncio
 async def test_add_success(session):
     company = Company(name="test", company_id=1)
@@ -12,7 +13,9 @@ async def test_add_success(session):
 
 @pytest.mark.asyncio
 async def test_add_duplicate(session):
-    await session.execute(text("INSERT INTO companies (name, company_id) VALUES ('test', 1)"))
+    await session.execute(
+        text("INSERT INTO companies (name, company_id) VALUES ('test', 1)")
+    )
     company = Company(name="test", company_id=1)
     with pytest.raises(Exception):
         await company_repo.add(company=company, session=session)
@@ -20,7 +23,9 @@ async def test_add_duplicate(session):
 
 @pytest.mark.asyncio
 async def test_get_all(session):
-    await session.execute(text("INSERT INTO companies (name, company_id) VALUES ('test', 1)"))
+    await session.execute(
+        text("INSERT INTO companies (name, company_id) VALUES ('test', 1)")
+    )
     companies = await company_repo.get_all(session=session)
     assert len(companies) == 1
 
